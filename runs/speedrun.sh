31117a6102417930defba84721ee3f7a7fcae7ed
Ding, C <c_ding@foxmail.com>
2026-04-12 02:26:35 +0800

    xiaoguo

--- runs/speedrun.sh
+++ runs/speedrun.sh
@@ -12,7 +12,7 @@
 
 # Default intermediate artifacts directory is in ~/.cache/nanochat
 export OMP_NUM_THREADS=1
-export NANOCHAT_BASE_DIR="/data/dingchun/.cache/nanochat"
+export NANOCHAT_BASE_DIR="/data/dingchun/nanochat-cache/nanochat"
 # Reduce CUDA allocator fragmentation for large training runs.
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
 mkdir -p $NANOCHAT_BASE_DIR
@@ -55,37 +55,37 @@ python -m nanochat.report reset
 # so we download 2e9 / 250e6 = 8 data shards at this point
 # each shard is ~100MB of text (compressed), so this is about ~800MB of data on disk
 # look at dev/repackage_data_reference.py for details on how this data was prepared
-python -m nanochat.dataset -n 8
+#python -m nanochat.dataset -n 8
 # Immediately also kick off downloading more shards in the background while tokenizer trains
 # Approximately 350 shards are needed for 10B tokens of data for pretraining.
 # The maximum total number of shards available in the entire dataset is 1822.
-python -m nanochat.dataset -n 370 &
-DATASET_DOWNLOAD_PID=$!
+#python -m nanochat.dataset -n 370 &
+#DATASET_DOWNLOAD_PID=$!
 # train the tokenizer with vocab size 2**15 = 32768 on ~2B characters of data
 python -m scripts.tok_train
 # evaluate the tokenizer (report compression ratio etc.)
-python -m scripts.tok_eval
+#python -m scripts.tok_eval
 
 # -----------------------------------------------------------------------------
 # Base model (pretraining)
 echo "Waiting for dataset download to complete..."
-wait $DATASET_DOWNLOAD_PID
+#wait $DATASET_DOWNLOAD_PID
 
 # d26 model (slightly undertrained to beat GPT-2 => decrease data:params ratio from compute optimal 10.5 (default) to 8.25)
-torchrun --standalone --nproc_per_node=4 -m scripts.base_train -- --depth=24 --target-param-data-ratio=8.25 --device-batch-size=1 --run=$WANDB_RUN
+torchrun --standalone --nproc_per_node=4 -m scripts.base_train -- --depth=24 --target-param-data-ratio=8.25 --device-batch-size=2 --eval-every=0 --core-metric-every=9999 --run=$WANDB_RUN
 # evaluate the model: CORE metric, BPB on train/val, and draw samples
-torchrun --standalone --nproc_per_node=4 -m scripts.base_eval -- --device-batch-size=1
+#torchrun --standalone --nproc_per_node=4 -m scripts.base_eval -- --device-batch-size=1
 
 # -----------------------------------------------------------------------------
 # SFT (teach the model conversation special tokens, tool use, multiple choice)
 
 # download 2.3MB of synthetic identity conversations to impart a personality to nanochat
 # see dev/gen_synthetic_data.py for details on how this data was prepared and to get a sense of how you can easily tune it
-curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
+#curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
 
 # run SFT and eval the model
-torchrun --standalone --nproc_per_node=4 -m scripts.chat_sft -- --device-batch-size=1 --run=$WANDB_RUN
-torchrun --standalone --nproc_per_node=4 -m scripts.chat_eval -- -i sft
+#torchrun --standalone --nproc_per_node=4 -m scripts.chat_sft -- --device-batch-size=1 --run=$WANDB_RUN
+#torchrun --standalone --nproc_per_node=4 -m scripts.chat_eval -- -i sft
 
 # chat with the model over CLI! Leave out the -p to chat interactively
 # python -m scripts.chat_cli -p "Why is the sky blue?"
@@ -96,4 +96,4 @@ torchrun --standalone --nproc_per_node=4 -m scripts.chat_eval -- -i sft
 # -----------------------------------------------------------------------------
 # Generate the full report by putting together all the sections
 # report.md is the output and will be copied to current directory for convenience
-python -m nanochat.report generate
+#python -m nanochat.report generate
